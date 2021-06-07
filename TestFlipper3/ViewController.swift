a7d81116e377fe799cce06f4480811516988f57b
andika <andikapratama@gmail.com>
2021-06-07 15:14:42 +0700

    empty project with flippper use_frameworks!

---
+++ TestFlipper3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFlipper3
+//
+//  Created by Andika Pratama on 11/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
